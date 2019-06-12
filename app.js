f28b4f2a234d1c19277f7c9d264340ef7520b017
emil-marlo <zenlord5.er@gmail.com>
2019-06-12 15:55:18 +0800

    Add post sucess route

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
 const app = express();
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 //set static ss and js files
 app.use(express.static("public"));
@@ -13,6 +16,18 @@ app.use(express.static("public"));
 app.get("/", function(req,res){
   res.render("form");
 });
+
+app.post("/", function(req,res){
+  const name = req.body.fullName;
+
+  res.render("sucess", {Name: name});
+});
+
+
+
+
+
+
 
 app.listen(3002, function(){
   console.log("Server started running on port 3002.");
